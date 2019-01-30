618ba3baac8cdfd21728cb24e6f5b0223a844b4d
Marc Millstone <marc@allenai.org>
2019-01-30 10:59:18 -0800

    mnist example

---
+++ README.md
@@ -0,0 +1,76 @@
+# MNIST Beaker Example
+
+This is an example of how to train a Pytorch MNIST model in Beaker.
+
+## Vanilla usage:
+
+1. install pytorch and torchvision
+2. set the python path:
+
+```bash
+export PYTHONPATH=.
+```
+
+3. download the four MNIST files from [Yann Lecun's website](http://yann.lecun.com/exdb/mnist/) and put them somewhere
+4. run the main file
+
+```bash
+python beaker_pytorch/main.py \
+    --data-dir /path/to/data \
+    --output-dir /tmp/output \
+    --epochs 10
+```
+
+## Docker usage:
+
+1. build the docker image
+
+```bash
+docker build -t beaker_pytorch .
+```
+
+2. Download the MNIST data, as above
+3. run the docker image mounting the data and output directories
+
+```bash
+docker run -t \
+    -v /local/path/to/data:/data:ro \
+    -v /local/path/to/output:/output:rw \
+    beaker_pytorch \
+    python beaker_pytorch/main.py \
+        --data-dir /data \
+        --output-dir /output \
+        --epochs 10
+```
+
+## Beaker usage
+
+1. [Install Beaker](https://github.com/allenai/beaker/wiki/Getting-Started)
+2. You can choose to download the MNIST data above and create a new dataset or use the existing MNIST dataset named
+`mnist.1` located [here](http://beaker.allenai.org/ds/ds_siigqzxt4ctd)
+3. You can either rebuild and push the docker image above or use the existing docker image already avaliable at
+`896129387501.dkr.ecr.us-west-2.amazonaws.com/beaker-experiment-playground:mnist.v1`
+
+If you chose to rebuild use
+```bash
+beaker blueprint create -n "beaker_pytorch" --addr="beaker.allenai.org" beaker_pytorch
+```
+to create a blueprint.
+
+4. Run the experiment:
+
+```bash
+beaker experiment run \
+    --source mnist.1:/data \
+    --result-path /output \
+    --gpu-count=1 \
+    896129387501.dkr.ecr.us-west-2.amazonaws.com/beaker-experiment-playground:mnist.v1 \
+    python beaker_pytorch/main.py \
+        --data-dir /data \
+        --output-dir /output \
+        --epochs 10
+```
+or
+```
+beaker experiment run -f spec.yml
+```
---
+++ beaker_pytorch/mnist_data.py
@@ -0,0 +1,109 @@
+"""
+A simpler version of torchvision.datasets.mnist.MNIST
+that only has the features we need
+
+copied and modified from https://github.com/pytorch/vision/blob/master/torchvision/datasets/mnist.py
+"""
+from __future__ import print_function
+import torch.utils.data as data
+from PIL import Image
+import os
+import os.path
+import errno
+import torch
+import codecs
+
+
+class MNIST(data.Dataset):
+    """`MNIST <http://yann.lecun.com/exdb/mnist/>`_ Dataset.
+
+    Args:
+        root (string): Root directory of datasets
+        train (bool, optional): whether to load the training data or the test data
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+    """
+    def __init__(self, root, train=True, transform=None, target_transform=None):
+        self.root = os.path.expanduser(root)
+        self.transform = transform
+        self.target_transform = target_transform
+
+        if train:
+            image_file, label_file = 'train-images-idx3-ubyte', 'train-labels-idx1-ubyte'
+        else:
+            image_file, label_file = 't10k-images-idx3-ubyte', 't10k-labels-idx1-ubyte'
+
+        print(f"loading images from {image_file}")
+        self.data = read_image_file(os.path.join(self.root, image_file))
+
+        print(f"loading labels from {label_file}")
+        self.labels = read_label_file(os.path.join(self.root, label_file))
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], self.labels[index]
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img.numpy(), mode='L')
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.data)
+
+
+def get_int(b):
+    return int(codecs.encode(b, 'hex'), 16)
+
+
+def parse_byte(b):
+    if isinstance(b, str):
+        return ord(b)
+    return b
+
+
+def read_label_file(path):
+    with open(path, 'rb') as f:
+        data = f.read()
+        assert get_int(data[:4]) == 2049
+        length = get_int(data[4:8])
+        labels = [parse_byte(b) for b in data[8:]]
+        assert len(labels) == length
+        return torch.LongTensor(labels)
+
+
+def read_image_file(path):
+    with open(path, 'rb') as f:
+        data = f.read()
+        assert get_int(data[:4]) == 2051
+        length = get_int(data[4:8])
+        num_rows = get_int(data[8:12])
+        num_cols = get_int(data[12:16])
+        images = []
+        idx = 16
+        for l in range(length):
+            img = []
+            images.append(img)
+            for r in range(num_rows):
+                row = []
+                img.append(row)
+                for c in range(num_cols):
+                    row.append(parse_byte(data[idx]))
+                    idx += 1
+        assert len(images) == length
+        return torch.ByteTensor(images).view(-1, 28, 28)
